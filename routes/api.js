4fefa6d3cd2be0cdac8f42ad0bfdd9fc965425ae
Glitch (time-ninth-truffle) <none>
2020-04-27 03:01:37 +0000

    ⏲🏛 Checkpoint ./server.js:15054522/230 ./routes/api.js:15054522/408

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,21 @@
 *
 */
 
+/* [Stock Price Checker API]
+
+  Since all reliable stock price APIs require an API key, 
+  we've built a workaround. Use https://repeated-alpaca.glitch.me/ 
+  to get up-to-date stock price information without needing 
+  to sign up for your own key.
+
+Usage:
+  GET https://repeated-alpaca.glitch.me/v1/stock/[symbol]/quote
+
+Where:
+  symbol = msft | goog | aapl | ...
+
+*/
+
 'use strict';
 
 var expect = require('chai').expect;
