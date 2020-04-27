964060bbf49039e646347e84ccd2cd4095a4e777
Glitch (time-ninth-truffle) <none>
2020-04-27 05:43:37 +0000

    👩‍👩‍👦🕋 Checkpoint ./routes/api.js:15054522/9904

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
 /* [Stock Price Checker API]
 
@@ -21,13 +21,13 @@ Where:
 
 */
 
-'use strict';
+"use strict";
 
-var expect       = require('chai').expect;
-var MongoClient  = require('mongodb');
-var axios        = require('axios');
+var expect = require("chai").expect;
+var MongoClient = require("mongodb");
+var axios = require("axios");
 
-//const CONNECTION_STRING = process.env.DB; 
+//const CONNECTION_STRING = process.env.DB;
 //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 // Mongoose
 var mongoose = require("mongoose");
@@ -36,56 +36,67 @@ const likeSchema = new Schema({
   stock: { type: String, required: true },
   ip: { type: String, required: true }
 });
-const Like = mongoose.model('Like', likeSchema);
+const Like = mongoose.model("Like", likeSchema);
 
-module.exports = (app) => {
+module.exports = app => {
   mongoose.set("useFindAndModify", false);
   mongoose.connect(process.env.DB);
-  
-  
-  
-  app.route('/api/stock-prices')
-    .get((req, res) => {
-      const stocks = req.query.stock ? Array.isArray(req.query.stock) ? req.query.stock : [req.query.stock] : null; // can be an array: Array.isArray(stock)
-      const like = req.query.like === 'true';
-      const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-      
-      if(!stocks || stocks.length < 1) res.status(400).send('no stock specified');
-    
-      const 
-    
-      // Get stock info from api
-      axios
-        .all(stocks.map(stock => axios.get(`https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`)))
-        .then(responses => {
-        
-          // Get all the successful stock infos
-          const stockInfos = responses.reduce((infos, res) => {
-            return res.status === 200 ? infos.concat({
+
+  app.route("/api/stock-prices").get((req, res) => {
+    const stocks = req.query.stock
+      ? Array.isArray(req.query.stock)
+        ? req.query.stock
+        : [req.query.stock]
+      : null; // can be an array: Array.isArray(stock)
+    const like = req.query.like === "true";
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+    if (!stocks || stocks.length < 1)
+      res.status(400).send("no stock specified");
+
+    const handleApiResponses = responses => {
+      // Get all the successful stock infos
+      const stockInfos = responses.reduce((infos, res) => {
+        return res.status === 200
+          ? infos.concat({
               stock: res.data.symbol,
               price: res.data.latestPrice
-            }) : infos;            
-          }, []);
-        
-          // Access the database for likes
-          Promise
-            .all(stockInfos.map(info => Like.find({ stock: info.stock }).exec()))
-            .then(responses => {
-              console.log(responses)
-              if(like){
-                // Save stock / increment likes
-                
-              } else{
-                
-                // Use the likes from responses, 0 if null
-                return res.json({ 
-                  stockData: stockInfos.length === 1 
-                  ? { ...stockInfos[0], likes: 0 } 
-                  : stockInfos.map(info => ({ ...info, likes: 0 })) });
-              }
             })
-        
-        })
-    });
-    
+          : infos;
+      }, []);
+
+      // Access the database for likes
+      const handleDbFindResponses = responses => {
+        console.log(responses);
+        if (like) {
+          // Save stock / increment likes
+          const updPromises = stockInfo.map(info => {
+            new Like({ stock: info.stock, ip })
+          })
+          
+        } else {
+          // Use the likes from responses, 0 if null
+          return res.json({
+            stockData:
+              stockInfos.length === 1
+                ? { ...stockInfos[0], likes: 0 }
+                : stockInfos.map(info => ({ ...info, likes: 0 }))
+          });
+        }
+      };
+      
+      Promise.all(
+        stockInfos.map(info => Like.find({ stock: info.stock }).exec())
+      ).then(handleDbFindResponses);
+    };
+
+    // Get stock info from api
+    axios
+      .all(
+        stocks.map(stock =>
+          axios.get(`https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`)
+        )
+      )
+      .then(handleApiResponses);
+  });
 };
