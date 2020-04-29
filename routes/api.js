a71c651f6cfdfcd4bad473236403e025f8d7373e
liamst19 <liamtstko20@gmail.com>
2020-04-29 16:04:05 +0900

    light refactoring and cleanup

--- routes/api.js
+++ routes/api.js
@@ -28,6 +28,20 @@ const axios = require('axios')
 
 const Like = require('../models/like')
 
+// Get Stock data from external api
+const getStockDataFromAPI = async stocks => {
+  const apiStockData = await axios.all(stocks.map(stock => axios.get(`https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`)))
+
+  return apiStockData.reduce((infos, res) => {
+    return res.status === 200
+      ? infos.concat({
+        stock: res.data.symbol,
+        price: res.data.latestPrice
+      })
+      : infos
+  }, [])
+}
+
 module.exports = app => {
 
   app.route('/api/stock-prices').get( async (req, res) => {
@@ -40,43 +54,37 @@ module.exports = app => {
     const like = req.query.like === 'true'
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
-    if (!stocks || stocks.length < 1)
-      res.status(400).send('no stock specified')
+    if (!stocks || stocks.length < 1) res.status(400).send('no stock specified')
 
-    const apiResponses = await axios.all(stocks.map(stock => axios.get(`https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`)))
+    // Get stock info from external api
+    const stockInfos = await getStockDataFromAPI(stocks)
 
-    // Get all the successful stock infos
-    const stockInfos = apiResponses.reduce((infos, res) => {
-      return res.status === 200
-        ? infos.concat({
-          stock: res.data.symbol,
-          price: res.data.latestPrice
-        })
-        : infos
-    }, [])
-
-    console.log('find data from db')
-    const dbFindResponses = await Promise.all(stockInfos.map(info => Like.find({ stock: info.stock }).exec()))
+    // Get Likes from Database
+    const stockLikes = await Promise.all(stockInfos.map(info => Like.find({ stock: info.stock }).exec()))
 
     // Reduce responses to an array of individual stocks and their counts
-    const likesCounts = dbFindResponses.reduce((stocks, res) => {
+    const likesCounts = stockLikes.reduce((stocks, res) => {
       return res && res.length > 0 ? stocks.concat({ stock: res[0].stock, count: res.length }) :stocks
     }, [])
 
+    // Checks whether the stock has already been 'liked' by the ip
     const alreadyLiked = stock => {
-      return stock && dbFindResponses.some(res => res[0] && res[0].stock === stock && res.some(l => l.ip === ip))
+      return stock && stockLikes.some(res => res[0] && res[0].stock === stock && res.some(l => l.ip === ip))
     }
 
+    // Gets the number of likes, incremented if the request included 'like=true'
     const getLikesCount = (info) => {
       const incr = like && info && !alreadyLiked(info.stock) ? 1 : 0
       const stock = info ? likesCounts.filter(s => s.stock === info.stock)[0] : null
       return stock ? stock.count + incr : incr
     }
 
-    const cntArr = likesCounts.map(s => s.count)
-    const relLikes = Math.max(...cntArr) - Math.min(...cntArr)
+    // calculate rel_likes
+    const countArray = likesCounts.map(s => s.count)
+    const rel_likes = Math.max(...countArray) - Math.min(...countArray)
 
-    if(like){ // save stock/ip to db and return incremented
+    // Save stock/ip if query included 'like=true'
+    if(like){
       await Promise.all(stockInfos.map(info =>  {
         return Like.findOneAndUpdate(
           { stock: info.stock, ip },
@@ -90,7 +98,7 @@ module.exports = app => {
       stockData:
       stockInfos.length === 1
         ? { ...stockInfos[0], likes: getLikesCount(stockInfos[0]) }
-        : stockInfos.map(info => ({ stock: info.stock, price: info.price, rel_likes: relLikes }))
+        : stockInfos.map(info => ({ stock: info.stock, price: info.price, rel_likes }))
     })
   })
 }
